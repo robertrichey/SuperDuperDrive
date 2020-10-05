7a73731b595bf0e1f4a3347f3c48316b2b3aa9de
Robert Richey <bobrichey215@gmail.com>
2020-10-05 15:46:04 -0400

    Began backend. Added security config, login and signup controllers, user mapper, and authentication and user services

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -1,12 +1,21 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
 public class User {
-    private int userid;
+    private Integer userid;
     private String username;
     private String salt;
     private String password;
     private String firstname;
     private String lastname;
+
+    public User(Integer userid, String username, String salt, String password, String firstname, String lastname) {
+        this.userid = userid;
+        this.username = username;
+        this.salt = salt;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 
     public int getUserid() {
         return userid;
